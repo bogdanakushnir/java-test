257a777d54f9f5f7c59c334427c1f51bd3b8d03e
Bogdana <bogdana.kushnir@sonarsource.com>
2023-01-23 16:56:34 +0100

    just for testing

--- src/main/java/org/test/example/Main.java
+++ src/main/java/org/test/example/Main.java
@@ -3,6 +3,7 @@ import org.test.example.*;
 
 public class Main {
   public static void main(String[] args) throws Exception{
+    long j =02343533;
     String s;
     System.out.println("Hello world!");
     int i = 0;
