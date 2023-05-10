c3b1ff478c812040448fa99ebb1bddb213f7554c
Bogdana <bogdana.kushnir@sonarsource.com>
2023-05-10 13:49:44 +0200

    testing

--- src/main/java/org/test/example/Main.java
+++ src/main/java/org/test/example/Main.java
@@ -1,8 +1,23 @@
 package org.test.example;
+import java.util.Random;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.net.ftp.FTPClient;
 import org.test.example.*;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class Main {
+
+  static final String HEADER_SIGNATURE = "X-Hub-Signature";
   public static void main(String[] args) throws Exception{
+
+    FTPClient ftpClient = new FTPClient();
+
+    System.out.println(ftpClient.toString());
+    Random random = new Random();
+    System.out.println(random.nextInt());
+
+
     String s;
     System.out.println("Hello world!");
     int i = 0;
@@ -16,8 +31,14 @@ public class Main {
     ///sdfsf
     ///sdfsdf
 
-    String p;
+    String p = HEADER_SIGNATURE;
     String p1;
 
+    getSha();
+
+  }
+
+  private static SecretKeySpec getSha(){
+    return new SecretKeySpec("secret".getBytes(UTF_8), "HmacSHA1");
   }
 }
