5addf18a583dc509078be7298eb44885aa48d54e
Bogdana <bogdana.kushnir@sonarsource.com>
2023-05-12 11:34:50 +0200

    testing short-lived branch identical to the main

--- src/main/java/org/test/example/Main.java
+++ src/main/java/org/test/example/Main.java
@@ -1,23 +1,9 @@
 package org.test.example;
 import org.test.example.*;
-
 public class Main {
   public static void main(String[] args) throws Exception{
+    long j =02343533;
     String s;
     System.out.println("Hello world!");
-    int i = 0;
-    boolean f;
-
-    if (i==0){
-      f = true;
-    }
-
-    // sfsfsdf
-    ///sdfsf
-    ///sdfsdf
-
-    String p;
-    String p1;
-
   }
 }
