1dabd058f90ef6d56bf4bad873a25c7c665afafb
Bogdana <bogdana.kushnir@sonarsource.com>
2023-10-27 11:07:43 +0200

    testing short-lived branch

---
+++ src/main/java/org/test/example/MainSLBranch.java
@@ -0,0 +1,53 @@
+package org.test.example;
+
+public class MainSLBranch {
+  public static void main(String[] args) throws Exception{
+    long j =02343533;
+    String s;
+    System.out.println("Hello world!");
+    int i = 0;
+    boolean f;
+
+    if (i==0){
+      f = true;
+    }
+
+    String p;
+    String p1;
+
+  }
+
+  public static void main1(String[] args) throws Exception{
+    long j =02343533;
+    String s;
+    System.out.println("Hello world!");
+    int i = 0;
+    boolean f;
+
+    if (i==0){
+      f = true;
+    }
+
+    String p;
+    String p1;
+
+  }
+
+  private static void main2() {
+    long j =02343533;
+    String s;
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+    int i = 0;
+    boolean f;
+
+    if (i==0){
+      f = true;
+    }
+
+    String p;
+    String p1;
+
+  }
+}
