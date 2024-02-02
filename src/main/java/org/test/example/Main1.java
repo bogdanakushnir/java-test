cce24bccac0d91a58f64f835b454392442569a96
Bogdana <bogdana.kushnir@sonarsource.com>
2024-02-02 09:40:08 +0100

    testing

---
+++ src/main/java/org/test/example/Main1.java
@@ -0,0 +1,20 @@
+package org.test.example;
+
+public class Main1 {
+  public static void main(String[] args) throws Exception{
+    long j =02343533;
+    String s;
+    System.out.println("Hello world!");
+    int i = 0;
+    boolean f;
+
+    if (i==0){
+      f = true;
+    }
+
+    String p;
+//    String p1 = getPassword();
+//    System.out.println(p1);
+  }
+
+}
