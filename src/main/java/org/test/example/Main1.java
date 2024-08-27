0fe4eb1bba45aa24af81ba16ebd2e34af68308fa
Bogdana <bogdana.kushnir@sonarsource.com>
2024-08-27 14:43:18 +0200

    testing
    (cherry picked from commit 1fca3d3e7178fea63b5337c929ad31d76efa56ea)

--- src/main/java/org/test/example/Main1.java
+++ src/main/java/org/test/example/Main1.java
@@ -13,8 +13,41 @@ public class Main1 {
     }
 
     String p;
-//    String p1 = getPassword();
-//    System.out.println(p1);
+    String p1 = getPassword();
+    System.out.println(p1);
   }
 
+  private static String getPassword(){
+    String password = "askjdhk";
+    return password;
+  }
+
+
+  private static String getPassword2(){
+    String password = "askjdhk";
+    return password;
+  }
+
+  private static String smthngNew2(){
+    String password = "askjdhk";
+    String password1 = "askjdhk";
+    String password2 = "askjdhk";
+    String password3 = "askjdhk";
+    return password;
+  }
+
+  private static String empty(){
+
+    return null;
+  }
+//  private static String empty2(){
+//    String password = "askjdhk";
+//    return password;
+//  }  private static String empty3(){
+//    String password = "askjdhk";
+//    return password;
+//  }  private static String empty4(){
+//    String password = "askjdhk";
+//    return password;
+//  }
 }
